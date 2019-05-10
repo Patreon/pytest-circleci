7953b8061e4d2d59efd15ac94cda0c5e8ee94ab4
Evan Moses <emoses@patreon.com>
2019-05-10 15:56:12 -0700

    Don't import plugin before we add requirements

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@ import textwrap
 from setuptools import setup
 
 from pytest_circleci import __version__
-from pytest_circleci import plugin
 
 setup(
     name="pytest-circleci",
     version=__version__,
     description="py.test plugin for CircleCI",
-    long_description=textwrap.dedent(plugin.pytest_collection_modifyitems.__doc__),
+    long_description = textwrap.dedent("""
+    Use CircleCI env vars to determine which tests to run
+
+    - CIRCLE_NODE_TOTAL indicates total number of nodes tests are running on
+    - CIRCLE_NODE_INDEX indicates which node this is
+
+    Will run a subset of tests based on the node index.
+
+    """),
     author="Michael Twomey",
     author_email="mick@twomeylee.name",
     url="https://github.com/micktwomey/pytest-circleci",
